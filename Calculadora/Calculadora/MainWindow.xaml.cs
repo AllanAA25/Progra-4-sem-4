e5d8da6ce63df35a8a4882672a0423dad079e4ad
Laboratorios <aamador-aleman@outlook.com>
2018-10-13 10:52:24 -0600

    Proyecto Semana 5

---
+++ Calculadora/Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+           double Op1 = Double.Parse(txtOperador1.Text);
+           double Op2 = Double.Parse(txtOperador2.Text);
+           string resultado = txtResultado.Text;
+
+
+            logicaCalculadora laLogica = new logicaCalculadora();
+            txtResultado.Text = laLogica.RealizarOperacion(txtOperador1.Text, txtOperador2.Text, txtResultado.Text);
+/*
+            double result;     
+            switch (cbOperadores.Text)
+            {
+                case "Suma":
+                result = Double.Parse(txtOperador1.Text) + Double.Parse(txtOperador2.Text);
+                txtResultado.Text = result.ToString();
+                    break;
+                case "Resta":
+                    result = Double.Parse(txtOperador1.Text) - Double.Parse(txtOperador2.Text);
+                    txtResultado.Text = result.ToString();
+                    break;
+                case "Multiplicación":
+                    result = Double.Parse(txtOperador1.Text) * Double.Parse(txtOperador2.Text);
+                    txtResultado.Text = result.ToString();
+                    break;
+                case "División":
+                    result = Double.Parse(txtOperador1.Text) / Double.Parse(txtOperador2.Text);
+                    txtResultado.Text = result.ToString();
+                    break;          
+            }
+*/
+        }
+    }
+}
---
+++ Calculadora/Calculadora/logicaCalculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class logicaCalculadora
+    {
+        /// <summary>
+        /// Esta función es la cara de la interfaz para hacer el calculo de las operaciones.
+        /// </summary>
+        /// <param name="Operando1">Hilera de caractares que representa el primer operando</param>
+        /// <param name="Operando2">Hilera de caractares que representa el segundo operando</param>
+        /// <param name="elOperador">Hilera de caractares que representa el operando del resultado</param>
+        /// <returns></returns>
+        public string RealizarOperacion(string Operando1, string Operando2, string elOperador)
+        {
+            double primerOperando = Double.Parse(Operando1);
+            double segundoOperando = Double.Parse(Operando2);
+            double elResultado = RealizarOperacion(primerOperando, segundoOperando, elOperador);
+            string elResultadoConvertido = elResultado.ToString();
+            return elResultadoConvertido;
+        }
+        public double RealizarOperacion (double Operando1, double Operando2, EnumeradoOperadores elOperador)
+        {
+            double result= 0.0;
+            switch (elOperador)
+            {
+                case EnumeradoOperadores.Suma:
+                result = Operando1 + Operando2;
+                    break;
+                case EnumeradoOperadores.Resta:
+                    result = Operando1 - Operando2;
+                    break;
+                case EnumeradoOperadores.Multiplicacion:
+                    result = Operando1 * Operando2;
+                    break;
+                case EnumeradoOperadores.Division:
+                    result = Operando1 / Operando2;
+                    break;          
+            }
+            return result;
+}
+    }
+}
